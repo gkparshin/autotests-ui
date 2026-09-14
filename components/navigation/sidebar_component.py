3403775d7d0d23cdc2a066a12cad0761582f9413
German Parshin <germa.parshin@x5.ru>
2026-09-14 21:57:46 +0300

    Практикуемся в реализации PageComponent

--- components/navigation/sidebar_component.py
+++ components/navigation/sidebar_component.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Page
 from components.navigation.sidebar_list_item_component import SideBarListItemComponent
 import re
 
-class SideBarComponent(BaseComponent):
+class SidebarComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -6,23 +6,15 @@ from pages.create_course_page import CreateCoursePage
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state: CoursesListPage):
-        chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+        courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-        expect(courses_title).to_be_visible()
-        expect(courses_title).to_have_text('Courses')
+        courses_list_page.navbar.check_visible("username")
+        courses_list_page.sidebar.check_visible()
 
-        empty_view_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-        expect(empty_view_icon).to_be_visible()
+        courses_list_page.check_visible_empty_view()
+        courses_list_page.toolbar_view.check_visible()
 
-        empty_view_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-        expect(empty_view_title).to_be_visible()
-        expect(empty_view_title).to_have_text('There is no results')
-
-        empty_view_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-        expect(empty_view_description).to_be_visible()
-        expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
 
 @pytest.mark.courses
 @pytest.mark.regression
@@ -38,14 +30,13 @@ def test_create_course(
         create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
         create_course_page.check_visible_create_course_title()
         create_course_page.check_disabled_create_course_button()
-        create_course_page.check_visible_image_preview_empty_view()
-        create_course_page.check_visible_image_upload_view()
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
         create_course_page.check_visible_create_course_form(title='', description='', estimated_time='', max_score="0", min_score="0")
         create_course_page.check_visible_exercises_title()
         create_course_page.check_visible_create_exercise_button()
         create_course_page.check_visible_exercises_empty_view()
-        create_course_page.upload_preview_image(file='./testdata/files/image.png')
-        create_course_page.check_visible_image_upload_view()
+        create_course_page.image_upload_widget.upload_preview_image(file='./testdata/files/image.png')
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
         create_course_page.fill_create_course_form(
                 title=title,
                 estimated_time=estimated_time,
@@ -54,9 +45,8 @@ def test_create_course(
                 min_score=min_score
         )
         create_course_page.click_create_course_button()
-        courses_list_page.check_visible_courses_title()
-        courses_list_page.check_visible_create_course_button()
-        courses_list_page.check_visible_course_card(
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
                 title=title,
                 estimated_time=estimated_time,
                 max_score=max_score,
