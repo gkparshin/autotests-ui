c9e8fa7a0189671a3397605485fc37aec79c45d5
German Parshin <germa.parshin@x5.ru>
2026-09-16 21:04:18 +0300

    Наращивание тестовой базы и рефакторинг

--- elements/input.py
+++ elements/input.py
@@ -13,4 +13,4 @@ class Input(BaseElement):
 
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         locator = self.get_locator(nth, **kwargs)
-        expect(locator).to_have_text(value)
+        expect(locator).to_have_value(value)
