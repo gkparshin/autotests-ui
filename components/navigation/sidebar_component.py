bb53f217c781672b7ac87243fb984863df4d4f97
German Parshin <germa.parshin@x5.ru>
2026-09-16 19:50:52 +0300

    Практическое применение PageFactory в автотестах

--- components/navigation/sidebar_component.py
+++ components/navigation/sidebar_component.py
@@ -1,16 +1,16 @@
 from components.base_component import BaseComponent
 from playwright.sync_api import Page
 
-from components.navigation.sidebar_list_item_component import SideBarListItemComponent
+from components.navigation.sidebar_list_item_component import SidebarListItemComponent
 import re
 
 class SidebarComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.logout_list_item = SideBarListItemComponent(page, 'logout')
-        self.courses_list_item = SideBarListItemComponent(page, 'courses')
-        self.dashboard_list_item = SideBarListItemComponent(page, 'dashboard')
+        self.logout_list_item = SidebarListItemComponent(page, 'logout')
+        self.courses_list_item = SidebarListItemComponent(page, 'courses')
+        self.dashboard_list_item = SidebarListItemComponent(page, 'dashboard')
 
 
     def check_visible(self):
--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,6 +1,11 @@
+import re
+
+from playwright.sync_api import Page
+
 from components.authentication.registration_form_component import RegistrationFormComponent
+from elements.button import Button
+from elements.link import Link
 from pages.base_page import BasePage
-from playwright.sync_api import Page, expect
 
 
 class RegistrationPage(BasePage):
@@ -8,7 +13,12 @@ class RegistrationPage(BasePage):
         super().__init__(page)
 
         self.registration_form = RegistrationFormComponent(page)
-        self.registration_button = page.get_by_test_id('registration-page-registration-button')
+
+        self.login_link = Link(page, 'registration-page-login-link', 'Login')
+        self.registration_button = Button(page, 'registration-page-registration-button', 'Registration')
+
+    def click_login_link(self):
+        self.login_link.click()
 
     def click_registration_button(self):
         self.registration_button.click()
