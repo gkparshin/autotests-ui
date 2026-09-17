00ff53fdc33a97eede51b9d5c2fd1e3b448f286c
German Parshin <germa.parshin@x5.ru>
2026-09-17 21:06:20 +0300

    Интеграция Allure в автотесты

--- tests/authentification/test_authorization.py
+++ tests/authentification/test_authorization.py
@@ -1,13 +1,24 @@
 import pytest
-
+import allure
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.epics import AllureEpic
+from tools.allure.tags import AllureTag
 from pages.authentification.login_page import LoginPage
 from pages.authentification.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
-
+from allure_commons.types import Severity
 
 @pytest.mark.regression
 @pytest.mark.authorization
+@allure.tag(AllureTag.REGRESSION, AllureTag.AUTHORIZATION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTIFICATION)
+@allure.story(AllureStory.AUTHORIZATION)
 class TestAuthorization:
+        @allure.title('User login with correct email and password')
+        @allure.tag("USER_LOGIN")
+        @allure.severity(Severity.BLOCKER)
         def test_success_authorization(self, registration_page: RegistrationPage, dashboard_page: DashboardPage, login_page: LoginPage):
             registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
             registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
@@ -33,12 +44,19 @@ class TestAuthorization:
                         ('  ', 'password')
                 ]
         )
+
+        @allure.title('User login with wrong email or password')
+        @allure.tag(AllureTag.USER_LOGIN)
+        @allure.severity(Severity.CRITICAL)
         def test_wrong_email_or_password_authorization(self, login_page: LoginPage, email: str, password: str):
             login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
             login_page.login_form.fill(email=email, password=password)
             login_page.click_login_button()
             login_page.check_visible_wrong_email_or_password_alert()
 
+        @allure.title('Navigation from login page to registration page')
+        @allure.tag(AllureTag.NAVIGATION)
+        @allure.severity(Severity.NORMAL)
         def test_navigate_from_authorization_to_registration(self, login_page: LoginPage, registration_page: RegistrationPage):
                 login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
                 login_page.click_registration_link()
--- tests/authentification/test_registration.py
+++ tests/authentification/test_registration.py
@@ -1,11 +1,23 @@
+import allure
 import pytest
+from allure_commons.types import Severity
+
 from pages.authentification.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
-
+from tools.allure.tags import AllureTag
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.epics import AllureEpic
 
 @pytest.mark.regression
 @pytest.mark.registration
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTIFICATION)
+@allure.story(AllureStory.REGISTRATION)
 class TestRegistration:
+        @allure.title('Registration with correct email, username and password')
+        @allure.tag(AllureTag.REGRESSION, AllureTag.REGISTRATION)
+        @allure.severity(Severity.CRITICAL)
         def test_successful_registration(self, registration_page: RegistrationPage, dashboard_page: DashboardPage):
                 registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
                 registration_page.registration_form.fill(email='user@gmail.com', username='username', password='password')
