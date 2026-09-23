4dd6772e8a678e7378589e992be271c452756ef1
German Parshin <germa.parshin@x5.ru>
2026-09-23 11:41:08 +0300

    Добавление Playwright Trace Viewer в Allure отчет

--- components/courses/courses_list_toolbar_view_component.py
+++ components/courses/courses_list_toolbar_view_component.py
@@ -1,5 +1,6 @@
 import re
 
+import allure
 from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
@@ -12,10 +13,9 @@ class CoursesListToolbarViewComponent(BaseComponent):
         super().__init__(page)
 
         self.title = Text(page, 'courses-list-toolbar-title-text', 'Title')
-        self.create_course_button = Button(
-            page, 'courses-list-toolbar-create-course-button', 'Create course'
-        )
+        self.create_course_button = Button(page, 'courses-list-toolbar-create-course-button', 'Create course')
 
+    @allure.step("Check visible courses list toolbar view")
     def check_visible(self):
         self.title.check_visible()
         self.title.check_have_text('Courses')
